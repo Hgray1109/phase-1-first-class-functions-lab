c6dfb3dccc97c97984f8ba451022d4d66fafb7ee
Hashem Gray <Hgray1109@gmail.com>
2021-08-15 15:46:01 -0400

    commit tree 6fde83a354f61248156f7226f764a561457c5136

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers() {
+    return (drivers.slice(0,2))
+}
+
+function returnLastTwoDrivers() {
+    return (drivers.slice(2))
+}
+
+const selectingDrivers = ([returnFirstTwoDrivers, returnLastTwoDrivers])
+
+function createFareMultiplier(multiplier) {
+    return function (fare) {
+        const taxi = (multiplier*fare)
+        return taxi
+    }
+}
+
+const fareDoubler = (createFareMultiplier(2));
+
+const fareTripler = (createFareMultiplier(3));
+
+function selectDifferentDrivers(arrayOfDrivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(arrayOfDrivers) 
+}
+
+// x * Y = z
+// multiplier * fare = taxi 
